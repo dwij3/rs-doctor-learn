687deda678b351280b19eb7022dfaa497481e84c
Dwij Bhanderi <bhanderidwij.sanjaykumar@sprinklr.com>
2025-04-15 12:49:30 +0530

    minor update

--- next.config.ts
+++ next.config.ts
@@ -7,18 +7,19 @@ const rsDoctorEnabled = process.env.RS_DOCTOR === 'true';
 const nextConfig: NextConfig = {
   /* config options here */
   webpack: (config) => {
-    console.log('conf!!' , config)
     if(rsDoctorEnabled){
       if (config.name === 'client') {
         config.plugins.push(
           new RsdoctorWebpackPlugin({
             disableClientServer: true,
-          }),
+            mode: 'brief',
+          })
         );
       } else if (config.name === 'server') {
         config.plugins.push(
           new RsdoctorWebpackPlugin({
             disableClientServer: true,
+            mode: 'brief',
             output: {
               reportDir: './.next/server',
             },
